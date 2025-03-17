b7b27ef648d566175b9c389ba7850b76d3b4a716
Coding-Robin001 <codingade@gmail.com>
2025-03-17 13:30:23 +0000

    added depenedecy, openAPIConfiguration and app.yml config for inventory service

---
+++ inventory-service/src/main/java/com/robin/microservices/inventory_service/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.robin.microservices.inventory_service.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI inventoryServiceApi(){
+        return new OpenAPI()
+                .info(new Info().title("Inventory Service API")
+                        .description("Rest API for inventory service, a component of onlineShoppingMicroservices")
+                        .version("v0.0.1")
+                        .license(new License().name("apache 2.0")))
+                .externalDocs(new ExternalDocumentation().description("more on inventory service on this link")
+                        .url("https://inventory-service-dummy-url/docs"));
+    }
+}
