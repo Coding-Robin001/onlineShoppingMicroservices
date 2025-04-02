4b0edef8c22d3058b42edcd024fa36f38183d26a
Coding-Robin001 <codingade@gmail.com>
2025-04-02 11:31:04 +0100

    added a custom swagger fallback route too

--- api-gateway/src/main/java/com/robin/microservices/api_gateway/routes/GatewayConfig.java
+++ api-gateway/src/main/java/com/robin/microservices/api_gateway/routes/GatewayConfig.java
@@ -7,11 +7,10 @@ import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.util.RouteMatcher;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
-import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
+
 
 import java.net.URI;
 
@@ -66,8 +65,17 @@ public class GatewayConfig {
                                 .body("Service currently not available, try again later!")).build();
     }
 
+    @Bean
+    public RouterFunction<ServerResponse> swaggerFallbackRoute() {
+        return GatewayRouterFunctions.route("swaggerFallbackRoute")
+                .GET("/swagger-fallback", request ->
+                        ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                                .body("API Documentation is currently unavailable. Please try again later."))
+                .build();
+    }
+    
 
-        @Bean
+    @Bean
         public RouterFunction<ServerResponse> swaggerRoutes() {
             return GatewayRouterFunctions.route("swagger_routes")
                     .route(RequestPredicates.path("/api-docs/product"),
