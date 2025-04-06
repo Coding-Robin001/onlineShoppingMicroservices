de5678a23c719848fb0d3b37f2a5232d0791904b
Coding-Robin001 <codingade@gmail.com>
2025-04-06 08:20:11 +0100

    integrated circuit breaker functionalities into order-service through feign client interface and app.yml file

--- order-service/src/main/java/com/robin/microservices/order_service/client/InventoryClient.java
+++ order-service/src/main/java/com/robin/microservices/order_service/client/InventoryClient.java
@@ -1,6 +1,10 @@
 package com.robin.microservices.order_service.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,7 +12,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(value = "Inventory", url = "http://localhost:8085")
 public interface InventoryClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
+    @CircuitBreaker(name = "inventoryService", fallbackMethod = "fallbackIsInStock")
+    @Retry(name = "inventoryService")
+    @RateLimiter(name = "inventoryService")
+    @GetMapping("/api/inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 
+    default boolean fallbackIsInStock(String skuCode, Integer quantity, Throwable ex) {
+        System.err.println("Fallback triggered due to: " + ex.getMessage());
+        return false;
+    }
 }
+
+
+
