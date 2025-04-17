9c4a3049d4e94e9a5267a4f468c573b3d98a3041
Coding-Robin001 <codingade@gmail.com>
2025-04-17 10:42:01 +0100

    added orderPlacedEvent and service class to conusme kafka topic in notification-service project

---
+++ notification-service/src/main/java/com/robin/microservices/notification_service/placeOrder/OrderPlacedEvent.java
@@ -0,0 +1,37 @@
+package com.robin.microservices.notification_service.placeOrder;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class OrderPlacedEvent {
+    private String orderNumber;
+    private String email;
+
+//    public OrderPlacedEvent() {}
+//
+//    public OrderPlacedEvent(String email, String orderNumber) {
+//        this.email = email;
+//        this.orderNumber = orderNumber;
+//    }
+//
+//    public String getOrderNumber() {
+//        return orderNumber;
+//    }
+//
+//    public void setOrderNumber(String orderNumber) {
+//        this.orderNumber = orderNumber;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+}
--- notification-service/src/main/java/com/robin/microservices/notification_service/service/NotificationService.java
+++ notification-service/src/main/java/com/robin/microservices/notification_service/service/NotificationService.java
@@ -1,7 +1,47 @@
 package com.robin.microservices.notification_service.service;
 
+import com.robin.microservices.notification_service.placeOrder.OrderPlacedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class NotificationService {
+
+    private final JavaMailSender javaMailSender;
+
+    @KafkaListener(topics = "order-placed", groupId = "notification-group")
+    public void listen(final OrderPlacedEvent orderPlacedEvent) {
+        log.info("Consumed message from 'order-placed' topic: {}", orderPlacedEvent);
+
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+            helper.setFrom("robinSpringShop@email.com");
+            helper.setTo(orderPlacedEvent.getEmail());
+            helper.setSubject(String.format("Your order with order number %s was placed successfully", orderPlacedEvent.getOrderNumber()));
+            helper.setText(String.format("""
+                    Hi,
+
+                    Your order with order number %s was placed successfully.
+
+                    Best regards,
+                    RobinSpring Shop
+                    """, orderPlacedEvent.getOrderNumber()));
+        };
+
+        try {
+            javaMailSender.send(messagePreparator);
+            log.info("Order notification email sent successfully!");
+        } catch (MailException ex) {
+            log.error("An error occurred while sending the email notification", ex);
+            throw new RuntimeException("Failed to send email notification", ex);
+        }
+    }
 }
