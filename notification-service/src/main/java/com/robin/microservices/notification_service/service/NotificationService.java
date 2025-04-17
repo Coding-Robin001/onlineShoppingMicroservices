945aa0c0d40635b0e5e112c0d8d8dfb3ba549557
Coding-Robin001 <codingade@gmail.com>
2025-04-17 01:50:27 +0100

    replaced notification project/app issues with dependency resolving new project works better with no dependency issues

--- notification-service/src/main/java/com/robin/microservices/notification_service/service/NotificationService.java
+++ notification-service/src/main/java/com/robin/microservices/notification_service/service/NotificationService.java
@@ -1,44 +1,7 @@
 package com.robin.microservices.notification_service.service;
 
-import com.robin.microservices.notification_service.placeOrder.OrderPlacedEvent;
+import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationService {
-
-    
-
-    private final JavaMailSender javaMailSender;
-
-    @KafkaListener(topics = "order-placed")
-    public void listen(final OrderPlacedEvent orderPlacedEvent){
-        log.info("consumed message from order-placed topic ", orderPlacedEvent);
-//        send email to customer
-        MineMessagePreparator messagePreparator = mineMessage -> {
-            MineMessageHelper messageHelper = new MineMessageHelper(mineMessage);
-            messageHelper.setFrom("robinSpringShop@email.com");
-            messageHelper.setTo(orderPlacedEvent.getEmail());
-            messageHelper.setSubject(String.format("your order with orderNumber %s is paced successfully ", orderPlacedEvent.getOrderNumber()));
-            messageHelper.setText(String.format("""
-
-             Hi
-
-             your order with order number %s was placed successfully.
-
-             Best regards
-             robinSpring shop
-
-             """,
-                    orderPlacedEvent.getOrderNumber()
-            ));
-        };
-
-        try {
-            javaMailSender.send(messagePreparator);
-            log.info("order notification sent successfully!")
-        } catch (MailException){
-            log.error("an error occured while sending notification");
-            throw new RuntimeException("exception occured when sending notifications");
-        }
-
-    }
 }
