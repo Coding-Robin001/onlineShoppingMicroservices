c9950ad2e069d3e406d33ab722e9f66552047d53
Coding-Robin001 <codingade@gmail.com>
2025-05-03 00:41:49 +0100

    added depndencies for the grafana stack to 4 microservices

--- notification-service/src/main/java/com/robin/microservices/notification_service/config/ObservationConfig.java
+++ notification-service/src/main/java/com/robin/microservices/notification_service/config/ObservationConfig.java
@@ -2,11 +2,26 @@ package com.robin.microservices.notification_service.config;
 
 import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.observation.aop.ObservedAspect;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 
 @Configuration
 public class ObservationConfig {
+
+    private final ConcurrentKafkaListenerContainerFactory<?, ?> concurrentKafkaListenerContainerFactory;
+
+    public ObservationConfig(ConcurrentKafkaListenerContainerFactory<?, ?> factory) {
+        this.concurrentKafkaListenerContainerFactory = factory;
+    }
+
+    @PostConstruct
+    public void setObservationForKafkaTemplate() {
+        concurrentKafkaListenerContainerFactory.getContainerProperties().setObservationEnabled(true);
+    }
+
     @Bean
     ObservedAspect observedAspect(ObservationRegistry registry) {
         return new ObservedAspect(registry);
